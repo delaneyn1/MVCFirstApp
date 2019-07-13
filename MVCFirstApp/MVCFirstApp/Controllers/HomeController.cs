c45c78e4de1f67595ca56ca0d133fd6f75aac685
Nick Delaney <ndelaney17@outlook.com>
2019-07-13 16:51:42 -0400

    little start to the controller page

---
+++ MVCFirstApp/MVCFirstApp/Controllers/HomeController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace MVCFirstApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public string Index()
+        {
+            return "Hello World, this is ASP.Net MVC Tutorials";
+        }
+    }
+}
